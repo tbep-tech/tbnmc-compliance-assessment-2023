12855d0b20246e9a634d16cf4e6d9569dea40a53
fawda123 <mbafs2012@gmail.com>
2024-01-19 15:27:13 -0500

    hard code date #3

--- R/funcs.R
+++ R/funcs.R
@@ -16,9 +16,6 @@ headertab <- function(dt = NULL, maxyr ,fsz = 13){
 
   if(is.null(dt))
     dt <-  as.character(as.Date(as.numeric(Sys.Date()), origin = '1970-01-01'))
-
-  if(!is.null(dt) & is.numeric(dt))
-    dt <-  as.character(as.Date(as.numeric(Sys.Date()), origin = '1970-01-01'))
   
   totab <- tibble(
     first = c('TO:', '', 'FROM:', 'DATE:', 'SUBJECT:', 'cc', '', '', '', ''),
